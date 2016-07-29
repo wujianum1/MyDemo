4dd859d5dc5ab72ac438437b3aa398c49310787e
jiajia <Joker@jokerdemac.local>
2016-07-29 08:52:11 +0800

    Initial Commit

---
+++ MyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDemo
+//
+//  Created by Joker on 16/7/29.
+//  Copyright © 2016年 Joker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDemo
+//
+//  Created by Joker on 16/7/29.
+//  Copyright © 2016年 Joker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
